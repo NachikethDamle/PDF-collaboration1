34dc99ea9284994a4faaae192b63843577420783
NachikethDamle <nachiblr2001@gmail.com.com>
2025-06-01 09:40:05 +0530

    upload

--- pdf_collab_backend/pdf-collab-system/server/app.js
+++ pdf_collab_backend/pdf-collab-system/server/app.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+app.use('/uploads', express.static('uploads'));
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
